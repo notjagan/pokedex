56bbbc3672b7491e1931806c3bb4ff3d8b5afe08
Jagan Prem <notjagan@gmail.com>
2022-10-11 17:40:13 -0400

    Read config options from .toml file

--- cmd/pokedex/main.go
+++ cmd/pokedex/main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"database/sql"
+	"log"
+
+	"github.com/BurntSushi/toml"
+)
+
+type config struct {
+	Discord struct {
+		Token string
+	}
+	DB struct {
+		Path string
+	}
+}
+
+const ConfigFile = "config.toml"
+
+func hostBot(cfg config) error {
+	_, err := sql.Open("sqlite3", cfg.DB.Path)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	var cfg config
+	_, err := toml.DecodeFile(ConfigFile, &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = hostBot(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/notjagan/pokedex
 
 go 1.19
+
+require github.com/BurntSushi/toml v1.2.0 // indirect
